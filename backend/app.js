58380bb4da655d80fa463e33a7234e1400cf87bc
cyvber <TahaNurEddin@gmail.com>
2025-01-08 15:24:28 +0200

    issue fix

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+require("dotenv").config(); // Load environment variables
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
@@ -11,24 +11,30 @@ const brandRoutes = require("./routes/brandRoutes");
 const orderRoutes = require("./routes/orderRoutes");
 const uploadRoutes = require("./routes/uploadRoutes");
 
-const allowedOrigins = process.env.API_URL;
-app.use(cors({
-  origin: allowedOrigins, // Only allow this origin
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow these HTTP methods
-  credentials: true, // If cookies or credentials are needed
-}));
 // Initialize the app
 const app = express();
+
+// Environment Variables
+const mongo_url = process.env.MONGO_URL; // MongoDB connection string
+const allowedOrigins = process.env.API_URL
+
+// CORS Configuration
+app.use(
+  cors({
+    origin: allowedOrigins, // Only allow specified origins
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"], // Allowed HTTP methods
+    credentials: true, // Enable credentials (if required)
+  })
+);
+
+// Middleware
 app.use(express.json());
 
-dotenv.config()
-
-const mongo_url = process.env.MONGO_URL
-// Database connection
+// Database Connection
 mongoose
-    .connect(`${mongo_url}`, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log("Connected to MongoDB"))
-    .catch((err) => console.error("Failed to connect to MongoDB", err));
+  .connect(mongo_url, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Failed to connect to MongoDB", err));
 
 // Serve static files
 app.use("/images", express.static(path.join(__dirname, "upload/images")));
