5c1329b771c8c094136f25af37f61133ca99a475
cyvber <TahaNurEddin@gmail.com>
2025-01-08 16:13:35 +0200

    more changes

--- backend/app.js
+++ backend/app.js
@@ -16,17 +16,27 @@ const app = express();
 
 // Environment Variables
 const mongo_url = process.env.MONGO_URL; // MongoDB connection string
-const allowedOrigins = process.env.ALLOWED_ORIGINS ? process.env.ALLOWED_ORIGINS.split(",") : [];
+const allowedOrigins = process.env.ALLOWED_ORIGINS
+  ? process.env.ALLOWED_ORIGINS.split(",")
+  : [];
+
 
 // CORS Configuration
 app.use(
-  cors({
-    origin: allowedOrigins, // Only allow specified origins
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"], // Allowed HTTP methods
-    credentials: true, // Enable credentials (if required)
-  })
-);
-
+    cors({
+      origin: (origin, callback) => {
+        // Allow requests with valid origins or no origin (e.g., Postman)
+        if (!origin || allowedOrigins.includes(origin)) {
+          callback(null, true);
+        } else {
+          callback(new Error("Not allowed by CORS"));
+        }
+      },
+      methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+      credentials: true, // Enable credentials
+    })
+  );
+app.options("*", cors());
 // Middleware
 app.use(express.json());
 
