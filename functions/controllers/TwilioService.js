6188fe2a74fe2cb6d9fa77d7019c93e3ded6c691
cyvber <TahaNurEddin@gmail.com>
2025-02-13 06:55:02 -0500

    removed twilio secret history and made final changes

--- functions/controllers/TwilioService.js
+++ functions/controllers/TwilioService.js
@@ -1,6 +1,8 @@
 // Assuming this is inside twilioService.js
 
 const twilio = require('twilio');
+
+
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = new twilio(accountSid, authToken);
